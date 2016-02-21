f7058b11217beaa54e3263d9ddf3e214d961976a
James Shakespeare <j@jshakespeare.com>
2016-02-21 15:14:59 -0500

    saving cities

--- app/jobs/createCity.js
+++ app/jobs/createCity.js
@@ -9,13 +9,30 @@
  */
 
 var songkick = require('../services/songkick');
+var City = require('../models/city');
 
 var coords = process.argv[2];
 
 songkick.getMetroFromCoords(coords)
   .then(function(location) {
 
-    console.log('found:', location);
+    var params = {
+      name: location.metroArea.displayName,
+      metroId: location.metroArea.id
+    }
+
+    // Add the state if it's a US city
+    if(location.metroArea.state) {
+
+      params.name += ', ' + location.metroArea.state;
+    }
+
+    var city = new City(params);
+    return city.save();
+  })
+  .then(function() {
+
+    console.log('City saved');
   })
   .catch(function(err) {
 
