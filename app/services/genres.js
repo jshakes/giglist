2cfd713bb8754ed01ed452b6daefa4dbe553e5e7
James Shakespeare <j@jshakespeare.com>
2017-02-05 13:13:35 -0500

    add other genres

--- app/services/genres.js
+++ app/services/genres.js
@@ -2,6 +2,57 @@ const genres = [
   {
     name: 'Acoustic',
     tags: ['indie folk', 'sdfds']
+  },
+  {
+    name: 'Blues'
+  },
+  {
+    name: 'Country'
+  },
+  {
+    name: 'Electronic/Dance'
+  },
+  {
+    name: 'Indie'
+  },
+  {
+    name: 'Jazz'
+  },
+  {
+    name: 'Pop'
+  },
+  {
+    name: 'R&B and Soul'
+  },
+  {
+    name: 'Rap/Hip Hop'
+  },
+  {
+    name: 'Rock'
+  },
+  {
+    name: 'Folk'
+  },
+  {
+    name: 'Funk'
+  },
+  {
+    name: 'Heavy Rock'
+  },
+  {
+    name: 'Punk'
+  },
+  {
+    name: 'World'
+  },
+  {
+    name: 'Oldies'
+  },
+  {
+    name: 'Classical'
+  },
+  {
+    name: 'Other'
   }
 ];
 
