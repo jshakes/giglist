e7d190df5aff3265ceba72ec28c344af5311ac23
James Shakespeare <j@jshakespeare.com>
2017-02-11 19:57:59 -0500

    tweaking genres a bit

--- app/services/genres.js
+++ app/services/genres.js
@@ -20,7 +20,7 @@ const genres = [
   {
   	id: 4,
     name: 'Classical',
-  	tags: ['Classical', 'piano', 'contemporary classical', 'epic', 'neoclassical', 'baroque', 'composer', 'opera', 'modern classical', 'violin', 'orchestral', 'composers', 'post-classical', 'contemporary piano', 'neo-classical', 'cello', 'symphonic']
+  	tags: ['Classical', 'piano', 'contemporary classical', 'neoclassical', 'baroque', 'composer', 'opera', 'modern classical', 'violin', 'orchestral', 'composers', 'post-classical', 'contemporary piano', 'neo-classical', 'cello', 'symphonic']
   },
   {
   	id: 5,
@@ -35,77 +35,72 @@ const genres = [
   {
   	id: 7,
     name: 'Electronic/Dance',
-  	tags: ['electronic', 'experimental', 'instrumental', 'dance', 'chillout', 'electronica', 'industrial', 'psychedelic', 'trance', 'House', 'electro', 'techno', 'downtempo', 'idm', 'synthpop', 'noise', 'Drum and bass', 'darkwave', 'ebm', 'dubstep', 'Disco', 'drone', 'electropop', 'psytrance', 'party', 'progressive trance', 'synth pop', 'remix', 'dark electro', 'noise rock', 'club', 'glitch', 'trip hop', 'dancehall', 'deep house', 'minimal techno', 'Progressive House', 'breakcore', 'bass', 'dnb', 'vocal trance', 'Drum n Bass', 'Electroclash', 'eurodance', 'indietronica', 'electro house', 'synth', 'tech house', 'goa', '8-bit', 'chill out', 'jungle', 'ninja tune', 'big beat', 'new rave', 'psychill', 'electro-industrial', 'psychedelic trance', 'uplifting trance', 'Rave', 'chillwave']
+  	tags: ['electronic', 'experimental', 'instrumental', 'dance', 'chillout', 'electronica', 'industrial', 'psychedelic', 'trance', 'House', 'electro', 'synthpop', 'noise', 'Drum and bass', 'dubstep', 'Disco', 'drone', 'electropop', 'psytrance', 'party', 'progressive trance', 'synth pop', 'remix', 'noise rock', 'club', 'glitch', 'trip hop', 'dancehall', 'deep house', 'minimal techno', 'Progressive House', 'breakcore', 'bass', 'dnb', 'vocal trance', 'Drum n Bass', 'Electroclash', 'eurodance', 'indietronica', 'electro house', 'synth', 'goa', '8-bit', 'chill out', 'jungle', 'ninja tune', 'big beat', 'new rave', 'psychill', 'electro-industrial', 'psychedelic trance', 'uplifting trance', 'Rave', 'chillwave']
   },
   {
-  	id: 8,
+    id: 9,
+    name: 'House and Techno',
+    tags: ['techno', 'downtempo', 'idm', 'darkwave', 'ebm', 'dark electro', 'noise rock', 'club', 'glitch', 'trip hop', 'dancehall', 'deep house', 'minimal techno', 'Progressive House', 'tech house']
+  },
+  {
+  	id: 9,
     name: 'Folk',
   	tags: ['folk', 'folk rock', 'neofolk', 'indie folk', 'bluegrass', 'freak folk', 'folk-rock']
   },
   {
-  	id: 9,
+  	id: 10,
     name: 'Funk',
   	tags: ['funk', 'funky', 'groovy']
   },
   {
-  	id: 10,
+  	id: 11,
     name: 'Indie',
   	tags: ['alternative', 'indie', 'alternative rock', 'indie rock', 'chillout', 'psychedelic', 'post-rock', 'new wave', 'american', 'indie pop', 'emo', 'Grunge', 'britpop', 'synthpop', 'Mellow', 'new age', 'shoegaze', 'Lo-Fi', 'Garage Rock', 'dream pop', 'art rock', 'soft rock', 'Alt-country', 'emocore', 'indie folk', 'Dreamy', 'post-grunge', 'indietronica', 'chill out', 'underground', 'twee', 'freak folk', 'chillwave']
   },
   {
-  	id: 11,
+  	id: 12,
     name: 'Jazz',
   	tags: ['jazz', 'Smooth Jazz', 'Fusion', 'acid jazz', 'nu jazz', 'saxophone', 'Bossa Nova', 'vocal jazz', 'jazz fusion', 'free jazz', 'jazzy', 'jazz vocal', 'jazz piano', 'nu-jazz', 'trumpet', 'contemporary jazz', 'bebop']
   },
   {
-  	id: 12,
+  	id: 13,
     name: 'Oldies',
   	tags: ['new wave', 'oldies', 'Disco', 'swing', 'motown', 'Big Band']
   },
   {
-  	id: 13,
+  	id: 14,
     name: 'Pop',
   	tags: ['pop', 'indie pop', 'britpop', 'synthpop', 'pop rock', 'j-pop', 'electropop', 'dream pop', 'JPop', 'synth pop', 'k-pop', 'mpb', 'power pop', 'futurepop', 'Pop-Rock', 'Kpop']
   },
   {
-  	id: 14,
+  	id: 15,
     name: 'R&B and Soul',
   	tags: ['soul', 'rnb', 'rhythm and blues', 'r&b', 'gospel', 'Neo-Soul', 'urban']
   },
   {
-  	id: 15,
+  	id: 16,
     name: 'Rap/Hip Hop',
   	tags: ['Hip-Hop', 'rap', 'hip hop', 'underground hip-hop', 'breakbeat', 'hiphop', 'Gangsta Rap', 'underground hip hop', 'breaks', 'instrumental hip-hop', 'urban', 'turntablism']
   },
   {
-  	id: 16,
+  	id: 17,
     name: 'Reggae / Ska',
   	tags: ['reggae', 'ska', 'dub', 'Stoner Rock', 'dancehall', 'ska punk', 'stoner', 'roots reggae']
   },
   {
-  	id: 17,
+  	id: 18,
     name: 'Rock',
-  	tags: ['rock', 'alternative rock', 'classic rock', 'indie rock', 'hard rock', 'Progressive rock', 'british', 'punk rock', 'post-rock', 'new wave', 'emo', 'Psychedelic Rock', 'Grunge', 'britpop', 'ska', 'blues rock', 'pop rock', 'Progressive', 'shoegaze', 'J-rock', 'Stoner Rock', 'folk rock', 'Garage Rock', 'art rock', 'soft rock', 'rockabilly', 'glam rock', 'space rock', 'rock n roll', 'Rock and Roll', 'krautrock', 'post rock', 'Southern Rock', 'emocore', 'industrial rock', 'rapcore', 'Experimental Rock', 'christian rock', 'math rock', 'psychobilly', 'garage', 'post-grunge', 'jrock', 'power pop', 'stoner', 'Pop-Rock', 'powerpop', 'Surf', 'prog', 'folk-rock', 'melodic rock', 'Deutschrock']
+  	tags: ['rock', 'alternative rock', 'classic rock', 'indie rock', 'hard rock', 'Progressive rock', 'punk rock', 'post-rock', 'new wave', 'emo', 'Psychedelic Rock', 'Grunge', 'britpop', 'ska', 'blues rock', 'pop rock', 'Progressive', 'shoegaze', 'J-rock', 'Stoner Rock', 'folk rock', 'Garage Rock', 'art rock', 'soft rock', 'rockabilly', 'glam rock', 'space rock', 'rock n roll', 'Rock and Roll', 'krautrock', 'post rock', 'Southern Rock', 'emocore', 'industrial rock', 'rapcore', 'Experimental Rock', 'christian rock', 'math rock', 'psychobilly', 'garage', 'post-grunge', 'jrock', 'power pop', 'stoner', 'Pop-Rock', 'powerpop', 'Surf', 'prog', 'folk-rock', 'melodic rock', 'Deutschrock']
   },
   {
-  	id: 18,
+  	id: 19,
     name: 'Heavy Rock / Metal',
   	tags: ['metal', 'hard rock', 'black metal', 'death metal', 'hardcore', 'heavy metal', 'thrash metal', 'metalcore', 'Progressive metal', 'Melodic Death Metal', 'Power metal', 'emo', 'post-hardcore', 'doom metal', 'Gothic Metal', 'Gothic', 'screamo', 'Nu Metal', 'symphonic metal', 'grindcore', 'darkwave', 'folk metal', 'deathcore', 'industrial metal', 'alternative metal', 'Brutal Death Metal', 'hardcore punk', 'Gothic Rock', 'Sludge', 'Female fronted metal', 'Technical Death Metal', 'speed metal', 'viking metal', 'visual kei', 'noise rock', 'melodic hardcore', 'russian rock', 'post rock', 'atmospheric black metal', 'goth', 'depressive black metal', 'Post-Metal', 'melodic metal', 'mathcore', 'industrial rock', 'pagan metal', 'progressive death metal', 'melodic black metal', 'Symphonic Black Metal', 'math rock', 'groove metal', 'Nu-metal', 'Symphonic Rock', 'thrash', 'hair metal', 'goregrind', 'old school death metal', 'stoner metal', 'Doom', 'martial industrial', 'deathrock', 'raw black metal', 'NWOBHM', 'nsbm', 'Blackened Death Metal', 'sludge metal', 'Avant-garde Metal', 'hardstyle', 'post hardcore', 'heavy']
   },
   {
-  	id: 19,
+  	id: 20,
     name: 'Punk',
   	tags: ['punk', 'punk rock', 'post-punk', 'pop punk', 'hardcore punk', 'emocore', 'Crust', 'psychobilly', 'ska punk', 'Oi', 'Post punk', 'horror punk']
-  },
-  {
-  	id: 20,
-    name: 'World',
-  	tags: ['japanese', 'german', 'french', 'russian', 'swedish', 'polish', 'j-pop', 'Canadian', 'finnish', 'J-rock', 'latin', 'celtic', 'deutsch', 'anime', 'italian', 'spanish', 'brazilian', 'UK', 'irish', 'australian', 'norwegian', 'Korean', 'brasil', 'mpb', 'english', 'icelandic', 'dutch', 'african', 'world', 'World Music', 'salsa', 'Ukrainian', 'Czech', 'germany', 'Scottish', 'Asian', 'chinese', 'ethnic', 'brazil', 'japan', 'france', 'turkish', 'arabic', 'Sweden', 'Flamenco', 'Greek', 'danish', 'tango', 'england', 'samba', 'balkan', 'traditional', 'Gypsy', 'scandinavian']
-  },
-  {
-  	id: 21,
-    name: 'Other',
-  	tags: ['instrumental', 'psychedelic', 'lounge', 'chill', 'Avant-Garde', 'melancholic', 'melancholy', 'christian', 'christmas', 'USA', 'romantic', 'Ballad', 'dark', 'relax', 'vocal', 'smooth', 'guitar virtuoso', 'medieval', 'avantgarde', 'old school', 'spoken word']
   }
 ];
 
