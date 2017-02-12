1a6595b862b670ea501317fcc8f33a481cbffa14
James Shakespeare <j@jshakespeare.com>
2017-02-12 10:19:53 -0500

    genre test script fix

--- scripts/getGenre.js
+++ scripts/getGenre.js
@@ -1,8 +1,7 @@
 const genres = require('../app/services/genres');
-const lastfm = require('../app/services/lastfm');
 const artist = process.argv[2];
-lastfm.getArtistTagArray(artist)
+genres.getArtistGenres(artist)
 .then(function(tags) {
-  console.log(genres.getGenreFromTag(tags[0]));
+  console.log(tags);
   process.exit();
 });
