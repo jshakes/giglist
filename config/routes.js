8059c68866a0b1ef1be0051dca8a290ba3709908
James Shakespeare <j@jshakespeare.com>
2017-02-18 17:45:20 -0500

    cities controller naming goof

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app, config) {
-  const cities = require('../app/controllers/Cities');
+  const cities = require(config.root + '/app/controllers/cities');
   app.get('/', cities.index);
   app.get('/:citySlug([a-z-]+)', cities.city);
   app.get('/:citySlug([a-z-]+)/:genreSlug([a-z-]+)', cities.playlist);
