277abeabc957f9167f92850058165c972d4a6fe3
James Shakespeare <j@jshakespeare.com>
2017-02-18 21:11:22 -0500

    move caching functions into a lib file

---
+++ app/lib/cache.js
@@ -0,0 +1,48 @@
+var fs = require('fs-promise');
+
+var CACHE_DIR = './cache';
+
+var cache = {
+  tryCache: function(prefix) {
+    var CACHE_EXPIRES = 86400000; // 24 hours
+    return fs.readdir(CACHE_DIR)
+    .then(function(files) {
+      if(!files.length) {
+        throw('No cache available');
+      }
+      var cacheMinAge = Date.now() - CACHE_EXPIRES;
+      var validCache = files.find(function(file) {
+        // deal with the filename, not the extension
+        var fileName = file.split('.')[0];
+        // get the timestamp from the filename
+        var parts = fileName.split('_');
+        var cachePrefix = parts[0];
+        var cacheDate = parseInt(parts[1], 10);
+        return cachePrefix === prefix && cacheDate > cacheMinAge;
+      });
+      if(!validCache) {
+        throw('No cache available');
+      }
+      console.log('Found valid cache:', validCache, 'not fetching new data');
+      return fs.readFile(`./${CACHE_DIR}/${validCache}`)
+      .then(function(contents) {
+        return contents;
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  },
+  writeCache: function(fileName, contents) {
+    return fs.ensureDir(CACHE_DIR)
+    .then(function() {
+      return fs.writeFile(`./${CACHE_DIR}/${fileName}_${Date.now()}.json`, JSON.stringify(contents));
+    })
+    .then(function() {
+      console.log('Wrote to cache');
+      return contents;
+    });
+  }
+};
+
+module.exports = cache;
--- scripts/updateCity.js
+++ scripts/updateCity.js
@@ -9,9 +9,8 @@ var City = require('../app/models/city');
 var cities = require('../app/services/cities');
 var genres = require('../app/services/genres');
 var tracks = require('../app/services/tracks');
-var fs = require('fs-promise');
+var cache = require('../app/lib/cache');
 
-var CACHE_DIR = './cache';
 var id = process.argv[2];
 
 City.findById(id)
@@ -21,7 +20,7 @@ City.findById(id)
     city: city
   };
   // try and get the tracks from the cache first
-  return getCache('tracks', city.id)
+  return cache.tryCache(`tracks-${city.id}`)
   .then(function(cacheContents) {
     if(cacheContents) {
       var parsedCache = JSON.parse(cacheContents);
@@ -30,14 +29,14 @@ City.findById(id)
       });
     }
     return trackObj;
-  })
+  });
 })
 .then(function(trackObj) {
   if(trackObj.events) {
     return cities.dispenseTracksToPlaylists(trackObj);
   }
   else {
-    return getCache('events', trackObj.city.id)
+    return cache.tryCache(`events-${trackObj.city.id}`)
     .then(function(cacheContents) {
       if(cacheContents) {
         var parsedCache = JSON.parse(cacheContents);
@@ -59,7 +58,7 @@ City.findById(id)
     .then(genres.getEventGenres)
     .then(tracks.getArtistTracks)
     .then(function(tracks) {
-      return writeCache('tracks', trackObj.city.id, tracks);
+      return cache.writeCache(`tracks-${trackObj.city.id}`, tracks);
     })
     .then(cities.dispenseTracksToPlaylists)
   }
@@ -73,42 +72,3 @@ City.findById(id)
   process.exit();
 });
 
-function getCache(prefix, cityId) {
-  var CACHE_EXPIRES = 86400000; // 24 hours
-  return fs.readdir(CACHE_DIR)
-  .then(function(files) {
-    if(!files.length) {
-      throw('No cache available');
-    }
-    var cacheMinAge = Date.now() - CACHE_EXPIRES;
-    var validCaches = files.filter(function(file) {
-      var parts = file.split('-');
-      var cachePrefix = parts[0];
-      var cacheCityId = parts[1];
-      var cacheDate = parseInt(parts[2], 10);
-      return cachePrefix === prefix && cacheCityId === cityId && cacheDate > cacheMinAge;
-    });
-    if(!validCaches.length) {
-      throw('No cache available');
-    }
-    console.log('Found valid cache:', validCaches[0], 'not fetching new data');
-    return fs.readFile(`./${CACHE_DIR}/${validCaches[0]}`)
-    .then(function(contents) {
-      return contents;
-    });
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-}
-
-function writeCache(prefix, cityId, contents) {
-  return fs.ensureDir(CACHE_DIR)
-  .then(function() {
-    return fs.writeFile(`./${CACHE_DIR}/${prefix}-${cityId}-${Date.now()}`, JSON.stringify(contents));
-  })
-  .then(function() {
-    console.log('Wrote to cache');
-    return contents;
-  });
-}
