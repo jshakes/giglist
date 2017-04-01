c760efb9484f8cc93d7a607c0fdcaee76a73d85d
James Shakespeare <j@jshakespeare.com>
2017-04-01 19:26:09 -0400

    test suite for genres

---
+++ tests/genres.test.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+var genres = require('../app/services/genres');
+
+describe('Genre service methods', () => {
+  it('Returns the genre map as an array of objects', () => {
+    const expects = expect.arrayContaining([
+      expect.any(Object)
+    ]);
+    return expect(genres.getGenres()).toEqual(expects);
+  });
+  it('Returns an array of genre names as strings we will map to', () => {
+    const expects = expect.arrayContaining([
+      expect.any(String)
+    ]);
+    return expect(genres.getGenreNames()).toEqual(expects);
+  });
+  it('Returns an array of genre IDs for an artist by name', () => {
+    const expects = expect.arrayContaining([
+      expect.any(Number)
+    ]);
+    return genres._getArtistGenres('David Bowie')
+    .then((genreIds) => {
+      expect(genreIds).toEqual(expects);
+    });
+  });
+});   
