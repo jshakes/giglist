baba36827c17ef116998398abb72cbc9e7f1e9d1
James Shakespeare <j@jshakespeare.com>
2017-04-01 19:47:56 -0400

    order matters in config inclusion

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-require('./config/express')(app);
 require('./config/rollbar')(app);
-require('./config/db')(app);
 require('./config/routes')(app);
+require('./config/express')(app);
+require('./config/db')(app);
 require('./config/cron')(app);
 
 module.exports = app;
