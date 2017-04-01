35c4a0cabf5249b52714b13cf133c462fe96f700
James Shakespeare <j@jshakespeare.com>
2017-04-01 19:20:38 -0400

    correctly plug blueird into mongoose

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 
 module.exports = function(app) {
-  mongoose.connect(process.env.MONGO_URL, {
-    promiseLibrary: require('bluebird')
-  });
+  mongoose.Promise = require('bluebird');
+  mongoose.connect(process.env.MONGO_URL);
   var db = mongoose.connection;
   db.on('error', function () {
     throw new Error('unable to connect to database at ' + process.env.MONGO_URL);
