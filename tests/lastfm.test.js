793b2b2f468317c03bae45f6c623b825f508b778
James Shakespeare <j@jshakespeare.com>
2017-04-01 18:18:51 -0400

    test suite for lastfm service

---
+++ tests/lastfm.test.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+var lastfm = require('../app/services/lastfm')();
+
+describe('Last.fm service methods', () => {
+  it('Gets an array of genre tags from an artist name', () => {
+    const expects = expect.arrayContaining([
+      expect.any(String)
+    ]);
+    return lastfm.getArtistTagArray('david bowie')
+    .then((tags) => {
+      expect(tags).toEqual(expects);
+    });
+  }, 10000);
+});
