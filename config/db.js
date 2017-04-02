8869bffbadb9e1dd0f61b210b38580b92c588438
James Shakespeare <j@jshakespeare.com>
2017-04-02 14:54:27 -0400

    refactor db conf file

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
 
-module.exports = function(app) {
-  mongoose.Promise = require('bluebird');
+module.exports = (app) => {
+  mongoose.Promise = Promise;
   mongoose.connect(process.env.MONGO_URL);
-  var db = mongoose.connection;
-  db.on('error', function () {
+  const db = mongoose.connection;
+  db.on('error', () => {
     throw new Error('unable to connect to database at ' + process.env.MONGO_URL);
   });
   return db;
